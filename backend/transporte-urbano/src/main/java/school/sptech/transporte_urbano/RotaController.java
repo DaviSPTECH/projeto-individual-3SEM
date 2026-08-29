94f015fddd75166a6b7133969e844b572919d1ae
DaviSPTECH <davi.pereira@sptech.school>
2026-08-29 15:35:11 -0300

    post

--- backend/transporte-urbano/src/main/java/school/sptech/transporte_urbano/RotaController.java
+++ backend/transporte-urbano/src/main/java/school/sptech/transporte_urbano/RotaController.java
@@ -4,11 +4,12 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @RestController
@@ -40,7 +41,50 @@ public class RotaController {
         catch (EmptyResultDataAccessException e) {
             return ResponseEntity.status(404).build();
         }
+    }
 
+    @PostMapping
+    public ResponseEntity<Rota> criarRota(@RequestBody Rota rota) {
 
+        if (rota.getDestino() == null || rota.getDestino().isBlank()) {
+            return ResponseEntity.status(400).build();
+        }
+
+        if (rota.getEstacaoInicial() == null || rota.getEstacaoInicial().isBlank()) {
+            return ResponseEntity.status(400).build();
+        }
+
+        if (rota.getEstacaoFinal() == null || rota.getEstacaoFinal().isBlank()) {
+            return ResponseEntity.status(400).build();
+        }
+
+        if (rota.getDuracaoMinutos() == null || rota.getDuracaoMinutos() <= 0) {
+            return ResponseEntity.status(400).build();
+        }
+
+        if (rota.getQtdBaldeacoes() == null || rota.getQtdBaldeacoes() < 0) {
+            return ResponseEntity.status(400).build();
+        }
+
+        String sql = "insert into rota (destino, estacaoInicial, estacaoFinal, duracaoMinutos, qtdBaldeacoes) " +
+                "values (?, ?, ?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, rota.getDestino());
+            ps.setString(2, rota.getEstacaoInicial());
+            ps.setString(3, rota.getEstacaoFinal());
+            ps.setInt(4, rota.getDuracaoMinutos());
+            ps.setInt(5, rota.getQtdBaldeacoes());
+
+            return ps;
+        }, keyHolder);
+
+        Integer idGerado = keyHolder.getKeyAs(Integer.class);
+        rota.setId(idGerado);
+        return ResponseEntity.status(201).body(rota);
     }
 }
